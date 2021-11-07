94014778d3d4d1c07c5578bf83818ff67ad9e639
zintesar <zintesar@gmail.com>
2021-11-07 12:43:41 +0600

    v 0.3.2

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
-import Products from '../components/Product'
+import Product from '../components/Product'
 import products from '../products'
 
 const HomeScreen = () => {

f5eb9b8bddc0802e72ed35d8322ea839383e74be
zintesar <zintesar@gmail.com>
2021-11-15 01:11:11 +0600

    v 0.4.9

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 // import products from './data/products.js'
 
@@ -10,12 +11,14 @@ dotenv.config()
 
 connectDB()
 
-
 const app = express()
+
+
 
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
+
 
 app.use('/api/products', productRoutes)
 

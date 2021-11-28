9bb399721e4fb829a00b355d682f147710183e7f
zintesar <zintesar@gmail.com>
2021-11-28 21:15:22 +0600

    final

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,44 +1,62 @@
 import React, { useEffect } from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { listProducts } from '../actions/productActions'
+import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
+import Paginate from '../components/Paginate'
+import ProductCarousel from '../components/ProductCarousel'
+import Meta from '../components/Meta'
+import { listProducts } from '../actions/productActions'
 
+const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword
 
-const HomeScreen = () => {
+  const pageNumber = match.params.pageNumber || 1
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const productList = useSelector(state => state.productList)
-    const { loading, error, products } = productList
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products, page, pages } = productList
 
-    useEffect(() => {
-        dispatch(listProducts())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(listProducts(keyword, pageNumber))
+  }, [dispatch, keyword, pageNumber])
 
-    console.log(products)
-
-    return (
-        <div>
-            <h1>Latest Products</h1>
-
-            {loading ? (
-                <Loader></Loader>
-            ) : error ? (
-                <Message variant='danger'></Message>
-            ) : (
-                <Row>
-                    {products.map((product) => (
-                        <Col Col key={product._id} sm={12} md={6} lg={4} xl={3} >
-                            <Product product={product} />
-                        </Col>
-                    ))}
-                </Row>)}
-
-        </div >
-    )
+  return (
+    <>
+      <Meta />
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to='/' className='btn btn-light'>
+          Go Back
+        </Link>
+      )}
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ''}
+          />
+        </>
+      )}
+    </>
+  )
 }
 
 export default HomeScreen

084d828d1b7362cfb3e4fe2106a1a5a125b7075c
zintesar <zintesar@gmail.com>
2021-12-08 23:50:40 +0600

    v 0.7.2 20%

---
+++ backend/controllers/userController.js
@@ -0,0 +1,10 @@
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+// @desc    Auth user &get token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+
+
+})

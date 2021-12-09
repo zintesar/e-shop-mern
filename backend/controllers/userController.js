8efa15c472c1aa411956478c49c123cd696918c6
zintesar <zintesar@gmail.com>
2021-12-10 00:20:59 +0600

    v 0.7.2

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,24 @@ const authUser = asyncHandler(async (req, res) => {
 
     const { email, password } = req.body
 
-    res.send(email, password)
+    const user = await User.findOne({ email })
+
+    if (user && (await user.matchPassword((password)))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: null
+        })
+
+    } else {
+        res.status(401)
+        throw new Error('invalid email or password')
+
+    }
+
+    res.send({ email, password, })
 
 })
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 // import products from './data/products.js'
@@ -13,7 +14,7 @@ connectDB()
 
 const app = express()
 
-
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('API is running...')
@@ -21,6 +22,7 @@ app.get('/', (req, res) => {
 
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 const PORT = process.env.PORT || 5000
 

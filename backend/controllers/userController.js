33005bd0df49188677d16353107293a637076df9
zintesar <zintesar@gmail.com>
2021-12-12 00:51:07 +0600

    v 0.7.7

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,27 +35,33 @@ const authUser = asyncHandler(async (req, res) => {
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
 
-    const { email, password } = req.body
+    const { name, email, password } = req.body
 
-    const user = await User.findOne({ email })
+    const userExists = await User.findOne({ email })
 
-    if (user && (await user.matchPassword((password)))) {
-        res.json({
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        res.status(201).json({
             _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
             token: generateToken(user._id)
         })
-
     } else {
-        res.status(401)
-        throw new Error('invalid email or password')
-
+        res.status(404)
+        throw new Error('Invalid user data')
     }
-
-    res.send({ email, password, })
-
 })
 
 // @desc    Get user profile
@@ -80,4 +86,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 })
 
-export { authUser, getUserProfile }
+export { authUser, getUserProfile, registerUser }

a2d67413e118c9bee6bbae74b22e528d8ef973c2
zintesar <zintesar@gmail.com>
2021-12-17 23:34:20 +0600

    v 0.8.2 20%

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react'
+import { Form, FormControl, FormLabel } from 'react-bootstrap'
+import FormContainer from '../components/FormContainer'
 
 const LoginScreen = () => {
 
-    const [email, setemail] = useState('')
-    const [password, setpassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
 
     return (
 
-        <div >
-
-        </div >
+        <FormContainer>
+            <h1>Sign In</h1>
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId='email'>
+                    <FormLabel>Email Address</FormLabel>
+                    <FormControl type='email' placeholder='Enter email'></FormControl>
+                </Form.Group>
+            </Form>
+        </FormContainer>
     )
 }
 

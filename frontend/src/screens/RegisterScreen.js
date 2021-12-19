17b9185d5c6d515d3ee7ea5c70f1693c17d80c3a
zintesar <zintesar@gmail.com>
2021-12-19 23:47:34 +0600

    v 0.8.4 60%

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -37,8 +37,7 @@ const RegisterScreen = () => {
 
     const submitHandler = (e) => {
         e.preventDefault()
-        dispatch(login(email, password))
-
+        //Dispatch register
     }
 
 
@@ -46,10 +45,14 @@ const RegisterScreen = () => {
 
         <FormContainer>
 
-            <h1>Sign In</h1>
+            <h1>Sign Up</h1>
             {error && <Message variant='danger'>{error}</Message>}
             {loading && <Loader />}
             <Form onSubmit={submitHandler}>
+                <Form.Group controlId='name'>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl type='name' placeholder='Enter name' value={name} onChange={(e) => setName(e.target.value)}></FormControl>
+                </Form.Group>
                 <Form.Group controlId='email'>
                     <FormLabel>Email Address</FormLabel>
                     <FormControl type='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}></FormControl>
@@ -59,15 +62,19 @@ const RegisterScreen = () => {
                     <Form.Label>Password</Form.Label>
                     <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
                 </Form.Group>
+                <Form.Group controlId='confirmPassword'>
+                    <Form.Label>confirm Password</Form.Label>
+                    <Form.Control type='password' placeholder='Confirm password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></Form.Control>
+                </Form.Group>
 
                 <Button type='submit' variant='primary'>
-                    Sign In
+                    Sign Up
                 </Button>
 
                 <Row>
                     <Col>
-                        New Customer?{' '}
-                        <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}> </Link>
+                        Have an Account?{' '}
+                        <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}> </Link>
                     </Col>
                 </Row>
             </Form>

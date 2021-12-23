138be44bb673cbbe8679fe39a47bea9cb7926254
zintesar <zintesar@gmail.com>
2021-12-23 23:39:37 +0600

    v 0.8.6 30% Profile Screen Get user Details

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -81,3 +81,45 @@ export const register = (name, email, password) => async (dispatch) => {
     }
 
 }
+
+// start here vid 8.6 03:42
+export const rregister = (name, email, password) => async (dispatch) => {
+
+    try {
+        dispatch({
+            type: USER_REGISTER_REQUEST
+        })
+
+        const config = {
+            Headers: {
+                'Content_Type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.post('/api/users/', { name, email, password }, config)
+
+        dispatch({
+            type: USER_REGISTER_SUCCESS,
+            payload: data
+        })
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch (error) {
+
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        })
+
+    }
+
+}

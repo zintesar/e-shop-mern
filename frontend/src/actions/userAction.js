4c83f16156da7e3a2c6186a09fa5e81c9ffc68c5
zintesar <zintesar@gmail.com>
2022-01-02 23:59:25 +0600

    v 0.8.7 10%

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../constants/userConstants"
+import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_UPDATE_PROFILE_REQUEST } from "../constants/userConstants"
 
 export const login = (email, password) => async (dispatch) => {
 
@@ -97,6 +97,45 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
                 Authorization: `Bearer ${userInfo.token}`
             }
         }
+
+        // const { data } = await axios.get(`/api/users/${id}`, config)
+        const data = {}
+
+        dispatch({
+            type: USER_DETAILS_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+
+        dispatch({
+            type: USER_DETAILS_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        })
+
+    }
+
+}
+
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+
+    try {
+        dispatch({
+            type: USER_UPDATE_PROFILE_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState()
+        console.log(userInfo.name)
+        const config = {
+            Headers: {
+                'Content_Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.get()
 
         // const { data } = await axios.get(`/api/users/${id}`, config)
         const data = {}

a9f18b038c600e217151c9452c0dfcbe4da7b84c
zintesar <zintesar@gmail.com>
2022-02-02 00:25:29 +0600

    v0.10.7 Show Orders On Profile

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
-import { Button, Col, Form, FormControl, FormLabel, Row } from 'react-bootstrap'
+import { Button, Col, Form, FormControl, FormLabel, Row, Table } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import Loader from '../components/Loader'
@@ -94,10 +95,46 @@ const ProfileScreen = () => {
             </Col>
             <Col md={9}>
                 <h2> My Orders </h2>
-                {/* {loadingOrders ? <Loader></Loader> :errorOrders ? <Message variant='danger'> {errorOrders}</Message> 
-                : (
-
-                )} */}
+                {loadingOrders ? <Loader></Loader> : errorOrders ? <Message variant='danger'> {errorOrders}</Message>
+                    : (
+                        <Table striped bordered hover responsive className='table-sm'>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>DATE</th>
+                                    <th>TOTAL</th>
+                                    <th>PAID</th>
+                                    <th>DELIVERED</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {orders.map(order => (
+                                    <tr key={order._id}>
+                                        <td>{order.createdAt.substring(0, 10)}</td>
+                                        <td>{order.totalPrice}</td>
+                                        <td>{order.isPaid ? (
+                                            order.paidAt.substring(0, 10)
+                                        ) : (
+                                            <i className='fas fa-times' style={{ color: 'red' }}></i>
+                                        )}
+                                        </td>
+                                        <td>{order.isDelivered ? (
+                                            order.deliveredAt.substring(0, 10)
+                                        ) : (
+                                            <i className='fas fa-times' style={{ color: 'red' }}></i>
+                                        )}
+                                        </td>
+                                        <td>
+                                            <LinkContainer to={`/order/${order._id}`}>
+                                                <Button className='btn-sm' variant='light'>Details</Button>
+                                            </LinkContainer>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    )}
             </Col>
         </Row>
     )

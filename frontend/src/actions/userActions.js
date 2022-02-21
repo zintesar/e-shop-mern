8bf4989178976f2e4fbd97dc84972c8c5f94b4a6
zintesar <zintesar@gmail.com>
2022-02-21 23:19:26 +0600

    v 0.13.0 typo fix

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -42,7 +42,7 @@ export const logout = () => (dispatch) => {
     dispatch({ type: USER_LOGOUT })
     dispatch({ type: USER_DETAILS_RESET })
     dispatch({ type: ORDER_LIST_MY_RESET })
-    dispatch({ typr: USER_LIST_RESET })
+    dispatch({ type: USER_LIST_RESET })
 }
 
 export const register = (name, email, password) => async (dispatch) => {

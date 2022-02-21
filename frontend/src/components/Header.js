7feff32b932923017c37e2bccc849b04a52727b3
zintesar <zintesar@gmail.com>
2022-02-21 23:18:50 +0600

    v0.13.0 fix: logut not redirecting to home

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,16 +3,20 @@ import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { logout } from '../actions/userActions'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
     const logoutHandler = () => {
         dispatch(logout())
+        navigate('/')
+
     }
 
     return (
